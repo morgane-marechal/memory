f70bc0a53ae8d0ce416ab55f9241d0643362b404
morgane-marechal <morgane.marechal@laplateforme.io>
2023-01-12 15:29:05 +0100

    add choice for card number

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
 ?>
+
+<?php
+
+?>
 <head>
     <meta charset="utf-8"/>
     <link rel="stylesheet" type="text/css" href="style.css" />
@@ -11,6 +15,12 @@ session_start();
 <body>
     
     <?php include('header.php'); ?> 
+    <?php $num_for_win_game=count($permanent_array); //<-condition de fin de jeu quand ce tableau comporte le nombre total de carte
+    $total_card=$_SESSION['total_card'];
+    if($num_for_win_game===$total_card){
+        echo "<div id = 'win'>Bravo vous avez fini le jeu !<br></div>";
+}
+    echo $total_card;?>
     <?php include ('get-button.php'); ?>
 
 
@@ -21,78 +31,99 @@ session_start();
         ?>
     </div>
     <main>
-        <div id="room">
+            <?php if(empty($_SESSION['login'])){
+                echo "<div id='welcome_no_connect'>Vous devez être connecté pour jouer au Memory !</div>";
+            }
+            ?>
+            <?php if (isset($_SESSION['login'])&& !empty($_SESSION['login'])){?>
+
+        
             <div id="welcome">
                 <h1>Voulez-vous jouer au Memory ?</h1>
+            </div>
+            <div id="buttons">
+
+                <form id = "nb-pairs" action="" method="post">
+                    <label for="nombre de paires">Nombre de paires :</label>
+                    <SELECT name="nb_pairs" >
+                    <OPTION>3
+                    <OPTION>4
+                    <OPTION>5
+                    <OPTION>6
+                    <OPTION>7
+                    <OPTION>8
+                    <OPTION>9
+                    <OPTION>10
+                    <OPTION>11
+                    <OPTION>12
+                    </SELECT>
+                    <input class="submit" type="submit" value="Envoyer">
+                    <i class="small">* Toujours reset avant de lancer une nouvelle partie</i>
+                </form>
+
+                <form id="reset" action="" method="post">
+                    <input class="submit" name = "reset" type="submit" value="Reset">
+                </form>
 
             </div>
-        </div>
+        <?php } ?>
      
-
-        <div id="memory-card">
-        <!-- <img src="images/1.jpg"/> -->
-        
-        </div>
-
         <?php include 'Card-PDO.php'; ?>
         <div id="all-card">
         <?php
-        //$_SESSION['temporary_array']=[]; //pour réinitialiser le tableau
-        //$_SESSION['permanent_array']=[]; //pour réinitialiser le jeu
+    
+    //_______________pour reset session_____________
+
+    if(isset($_POST['reset'])){
+        $_SESSION['temporary_array']=[]; //pour réinitialiser le tableau
+        $_SESSION['permanent_array']=[]; //pour réinitialiser le jeu
+        $_SESSION['order_card']=[];
+        $_SESSION['memory_game'] = [];
+    }
+// echo $_SESSION['order_card'];
+// echo $_SESSION['memory_game'];
+// echo "order card : ".$_SESSION['order_card']."<br>";
 
         $temporary_array=$_SESSION['temporary_array'];
         $permanent_array=$_SESSION['permanent_array'];
-        var_dump($temporary_array);
-        echo "<- temporary array";
-        var_dump($permanent_array);
-        echo "<- permanent array";
-            $num_in_arr= count($temporary_array);
-            echo "Taille du tableau : ".$num_in_arr." <br>";
-           
+        $num_in_arr= count($temporary_array); // <- compte jusqu'à 2 pour ensuite vider le petit tableau
+        
+        $num_for_win_game=count($permanent_array); //<-condition de fin de jeu quand ce tableau comporte le nombre total de carte
+        $total_card=$_SESSION['total_card'];
+        //echo $total_card;
+        if($num_for_win_game===$total_card){
+                echo "<div id = 'win'>Bravo vous avez fini le jeu !<br></div>";
+        }
 
+  //______________________lancer la visualisation des cartes _____________________________          
 
+            if(($_SESSION['memory_game'] != "card_set")&&(isset($_POST['nb_pairs']))){
 
-
-            
-
-            if($_SESSION['memory_game'] != "card_set"){
-                    $display = [
-                        1 => '1.jpg',
-                        2 => '2.jpg',
-                        3 => '3.jpg',
-                        4 => '4.jpg',
-                        5 => '5.jpg',
-                        6 => '6.jpg',
-                        7 => '7.jpg',
-                        8 => '8.jpg',
-                        9 => '9.jpg',
-                        10 => '10.jpg',
-                        11 => '11.jpg',
-                        12=> '12.jpg',
-                        13 => '1.jpg',
-                        14 => '2.jpg',
-                        15 => '3.jpg',
-                        16 => '4.jpg',
-                        17 => '5.jpg',
-                        18 => '6.jpg',
-                        19 => '7.jpg',
-                        20 => '8.jpg',
-                        21 => '9.jpg',
-                        22 => '10.jpg',
-                        23 => '11.jpg',
-                        24 => '12.jpg'
-                    ];
+                 $nbpairs=$_POST['nb_pairs'];
+                 $display_test=[];
+                 for ($i = 1; $i <= $nbpairs; $i++) {
+                  $display_test[$i] = $i.'.jpg';
+                 }
+         
+                 for ($i = 1; $i <= $nbpairs; $i++) {
+                     $display_test[$nbpairs+$i] = ($i).'.jpg';
+                 }
+                    $display = $display_test;
                     shuffle($display);
+
                     $_SESSION['order_card'] = $display;
                     $_SESSION['memory_game'] = "card_set";
+                    $_SESSION['total_card']=$nbpairs*2;
                     foreach ($display as $key => $value){
                         $id = $key;
                         $faceon = "<img src = 'images/".$value."'/>";
                         $infoCard[$key] = new Game("$id","$faceon");
-                        echo "<form id ='board-card' titre = 'get' action='index.php'  method='get'> <div class='card'><button type='submit' value ='".$key."' name='button".$key."'>".$infoCard[$key]->getFaceOn()."</div></form>";
+                        echo "<form id ='board-card' titre = 'get' action='index.php'  method='get'> <div class='card'><button type='submit' value ='".$key."' name='button".$key."'>".$infoCard[$key]->getFaceOff()."</div></form>";
                     }
             }elseif(($_SESSION['memory_game'] == "card_set")){
-                    $display = $_SESSION['order_card'];
+                //echo $_SESSION['memory_game'];
+                   $display = $_SESSION['order_card'];
+                  //echo  var_dump($display);
                     foreach ($display as $key => $value){
                         $id = $key;
                         $faceon = "<img src = 'images/".$value."'/>";
@@ -100,7 +131,6 @@ session_start();
                         $infoCard[$key] = new Game("$id","$faceon");
                         $temporary_array=$_SESSION['temporary_array'];
                         $permanent_array=$_SESSION['permanent_array'];
-                        //$returnedCard = [ 1, 2,3,4,5];
                         if (in_array($key,$temporary_array)|| in_array($key,$permanent_array)) {
                             echo "<form id ='board-card' titre = 'get' action='index.php'  method='get'> <div class='card'><button type='submit' value ='".$key."' name='button".$key."'>".$infoCard[$key]->getFaceOn()."</div></form>";
 
@@ -109,10 +139,12 @@ session_start();
                         }
             }
 
+//____________________vérifier les cartes retournées et garder en mémoire les paires________________
+
             if($num_in_arr === 2){
                 $val1=$temporary_array[0];
                 $val2=$temporary_array[1];
-                echo "Les valeurs sont ".$val1."et".$val2.".";
+                //echo "Les valeurs sont ".$val1."et".$val2.".";
             //aller chercher les image dont les id correspondent aux valeurs et les comparer
             $card1 = $infoCard[$val1]->getFaceOn();
             $card2 = $infoCard[$val2]->getFaceOn();
@@ -124,17 +156,25 @@ session_start();
                     $permanent_array=[];
                    $new_permanent_array= array_merge ($temporary_array, $permanent_array);
                    $_SESSION['permanent_array']=$new_permanent_array;
-                   echo "Tableau permanent si deux cartes pareils";
-                   echo var_dump($new_permanent_array);
+                //    echo "Tableau permanent si deux cartes pareils";
+                //    echo var_dump($new_permanent_array);
                    $_SESSION['temporary_array']=[];
                 }elseif($_SESSION['set_array'] === "OK"){
-                    echo "GGGGGGGGGGGG";
                     $permanent_array=$_SESSION['permanent_array'];
                     $new_permanent_array= array_merge ($temporary_array, $permanent_array);
                    $_SESSION['permanent_array']=$new_permanent_array;
-                   echo "Tableau permanent si deux cartes pareils";
-                   echo var_dump($new_permanent_array);
+                //    echo "Tableau permanent si deux cartes pareils";
+                //    echo var_dump($new_permanent_array);
                    $_SESSION['temporary_array']=[];
+                   $num_for_win_game=count($new_permanent_array); //<-condition de fin de jeu quand ce tableau comporte le nombre total de carte
+                   $total_card=$_SESSION['total_card'];
+                   //echo $total_card;
+                   if($num_for_win_game===$total_card){
+                           echo "<br><div id = 'win'>Bravo vous avez fini le jeu !<br></div>";
+                           $endgame=true;
+                           $_SESSION['endgame']=$endgame;
+                   }
+
                 }
             }if(($card1===$card2) && ($val1===$val2)){
                 $_SESSION['temporary_array']=[];
@@ -143,56 +183,16 @@ session_start();
             if($card1 != $card2){
                 $_SESSION['temporary_array']=[];
             }
-
-
-
             }
-
-            /* ___________________________création d'un tableau de _SESSION pour les images retournées test ________*/
-
-            // echo  $infoCard[0]->toggleFlip();
-            // echo  $infoCard[0]->testFlip();
-            // echo "hello".$_GET["button0"];
-            // //     $card1 = $_GET["button0"];
-            // //    $card2 = $_GET["button1"];
-            // //    echo $card1."<br>".$card2;
-            // $faceoff = "<img src = 'images/back.jpg'/>";
-            // 
-            // echo  $infoCard[0]->getFaceOn();
-       
-
-    //    $infoCard[$key]->toggleFlip();  // _____pour conserver carte retournée pour la prochaine instanciation 
-    //    $isFlipped = $infoCard[$key]->getFlip(); // ______faire variable array de _session ?
-    //    //echo gettype($isFlipped);     //___ faire 2 array, un qui contient les 2 cartes temporaires et un qui contient cartes définitivement retournée
-    //    if($isFlipped === false){
-    //        echo 'La variable est sur false';
-    //    }elseif($isFlipped === true){
-    //        echo 'La variable est sur true';
-    //    }
    }
 
 
-       //echo $infoCard[10]->getFaceOn();
 
-
-
-        /* ___________________________pour comparer deux résultats de cartes, chercher du côtés de GET ________*/
     
         ?>
-
-
-<?php
-        
-        ?>
-  
         </div>
-        <h1>Ecrire quelque chose ....</h1>
-        <h1>Ecrire quelque chose ....</h1>
-        <h1>Ecrire quelque chose ....</h1>
-
-
-        </div>
-    </main>
+    </div>
+</main>
 
 
     <?php include('footer.php'); ?>
